25c33ddf6892d8bc25109df66af288ac62e84ef6
JEFRIN ELDHOS JOY <jefrinjoy123@gmail.com>
2026-09-19 15:05:24 +0530

    feat: add get club details api

--- backend/src/main/java/au/jefrin/club/dto/ClubResponse.java
+++ backend/src/main/java/au/jefrin/club/dto/ClubResponse.java
@@ -4,7 +4,6 @@ import au.jefrin.user.dto.UserResponse;
 import lombok.Builder;
 import lombok.Getter;
 import java.sql.Timestamp;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Getter
 @Builder
@@ -15,6 +14,5 @@ public class ClubResponse {
     private Timestamp dateOfCreation;
     private UserResponse createdBy;
     private int memberCount;
-    @JsonProperty("isMember")
-    private boolean isMember;
+    private Boolean isMember;
 }
